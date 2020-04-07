d1c1915083270e4f2b8525bb0b58b9217217aef3
Arturo Morales <45403642+TheBlackStoneWielder61@users.noreply.github.com>
2020-04-06 20:33:42 -0500

    Add files via upload

---
+++ gen_candidate.java
@@ -0,0 +1,16 @@
+
+public class gen_candidate {
+	
+	//INPUT:  Large itemets Lk-1, Hash table Hk
+	//OUTPUT: Candidate itemsets Ck
+	public void gen_candidate(){
+		//Declare Ck as an empty set
+		//For all elements i in Lk-1
+			//For all elements i+1 in Lk-1
+				//If the intersection between them is equal to k-2
+					//If it achieves minimum support 
+						// Add it to Ck
+		//Return Ck
+	}
+
+}
