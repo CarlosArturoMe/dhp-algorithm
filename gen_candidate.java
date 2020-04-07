53bd6c003e971038b1a42935d77ff21b95d87357
Arturo Morales <45403642+TheBlackStoneWielder61@users.noreply.github.com>
2020-04-06 21:52:15 -0500

    Update gen_candidate.java

--- gen_candidate.java
+++ gen_candidate.java
@@ -5,11 +5,15 @@ public class gen_candidate {
 	//OUTPUT: Candidate itemsets Ck
 	public void gen_candidate(){
 		//Declare Ck as an empty set
-		//For all elements i in Lk-1
-			//For all elements i+1 in Lk-1
-				//If the intersection between them is equal to k-2
-					//If it achieves minimum support 
-						// Add it to Ck
+		//For all itemsets i in Lk-1
+			//For all itemsets i+1 in Lk-1
+				//For elements e in i
+					//If e in i+1
+						//count++
+				//If count == k-2
+				//Merge i and i+1 in a set c (no duplicates)
+				//If c achieves minimum support and c not in Ck 
+					// Add it to Ck
 		//Return Ck
 	}
 
