186f82bd3c28d113f522c4dd7faee3e5245e12bb
Arturo Morales <45403642+TheBlackStoneWielder61@users.noreply.github.com>
2020-04-24 12:27:02 -0500

    Add files via upload

--- dhp.py
+++ dhp.py
@@ -13,7 +13,7 @@ def return_items_with_min_support_p1(item_set, transaction_list, min_support, fr
     _item_set = set()
     local_set = defaultdict(int)
     h2_set = defaultdict(int)
-
+    
     for item in item_set:
         for transaction in transaction_list:
             if item.issubset(transaction):
@@ -21,22 +21,22 @@ def return_items_with_min_support_p1(item_set, transaction_list, min_support, fr
                 local_set[item] += 1
             trans_set = set()
             for transaction_comp in transaction:
-                #print(transaction_comp)
-                trans_set.add(frozenset((transaction_comp,)))
+                trans_set.add(frozenset(transaction_comp))
+            ##DEBUG##
             #print("transaction: ",trans_set)
+            ##DEBUG##
             two_subsets = join_set(trans_set, 2)
-            #print("two_subsets: ",two_subsets)
             for element in two_subsets:
-                #print("element to h2_set: ",element)
                 h2_set[element] += 1
 
     for item, count in local_set.items():
-        support = float(count)/len(transaction_list)
-        #print(support)
-        if support >= min_support:
-                _item_set.add(item)
-    #print("_item_set: ",_item_set)
-    #print("h2_set: ",h2_set)
+            support = float(count)/len(transaction_list)
+            ##DEBUG##
+            #print(support)
+            ##DEBUG##
+            if support >= min_support:
+                    _item_set.add(item)
+
     return _item_set, h2_set
 
 def return_items_with_min_support(item_set, transaction_list, min_support, freq_set):
@@ -51,7 +51,9 @@ def return_items_with_min_support(item_set, transaction_list, min_support, freq_
 
     for item, count in local_set.items():
             support = float(count)/len(transaction_list)
+            ##DEBUG##
             #print(support)
+            ##DEBUG##
             if support >= min_support:
                     _item_set.add(item)
 
@@ -59,19 +61,21 @@ def return_items_with_min_support(item_set, transaction_list, min_support, freq_
 
 
 def join_set(item_set, length):
+    ##DEBUG##
     #print("item_set join_set: ")
     #print(item_set)
+    ##DEBUG##
     return set([i.union(j) for i in item_set for j in item_set if len(i.union(j)) == length])
 
 def gen_candidate(l_set, h_set, k, min_support,transaction_list):
     candidates_with_support = set()
-    #print("l_set: ",l_set)
     candidates = set([i.union(j) for i in l_set for j in l_set if len(i.intersection(j)) == k-2])
-    #print("candidates: ",candidates)
+    print("candidates: ",candidates)
     for c in candidates:
+        ##DEBUG##
         #print("c: ",c)
-        #if h_set[c] > 0:
-            #print(h_set[c])
+        #print(h_set[c])
+        ##DEBUG##
         support = float(h_set[c])/len(transaction_list)
         if support >= min_support:
             candidates_with_support.add(c)
@@ -89,15 +93,36 @@ def get_item_set_transaction_list(data_iterator):
     transaction_list = list()
     item_set = set()
     for record in data_iterator:
-        #print("record: ",record)
+        ##DEBUG##
+        # print("record: ",record)
+        ##DEBUG##
         transaction = frozenset(record)
         transaction_list.append(transaction)
         for item in transaction:
             item_set.add(frozenset([item]))
-    #print(item_set)
-    #print(transaction_list)
+    ##DEBUG##
+    # print(item_set)
+    # print(transaction_list)
+    ##DEBUG##
     return item_set, transaction_list
 
+#OUTPUT: Hash set Hk+1 and trimmed transaction transaction_double_prime  
+def make_hasht(transaction, index_k, hash_set_k):
+    hash_set_k1 = dict()
+    local_item_count = dict()
+    transaction_double_prime = set()
+    transaction_subsets = join_set(transaction, index_k +1)
+    for subset in transaction_subsets:
+        sub_subsets = join_set(subset, index_k)
+        for subset in sub_subsets:
+            if(True):    ##support(subset) >= min_sup 
+                continue ##hash_set_k1[subset] += 1
+            for item in subset: 
+                continue ##add item to local_item_count or add 1 
+    for item in transaction:
+        continue ##if local count > 0, add to transaction_double_prime 
+
+    return hash_set_k1, transaction_double_prime
 
 def run_AprioriDHP(data_iter, min_support, min_confidence,large):
     item_set, transaction_list = get_item_set_transaction_list(data_iter)
@@ -105,16 +130,18 @@ def run_AprioriDHP(data_iter, min_support, min_confidence,large):
     large_set = dict()
     hash_set = dict()
     one_c_set, two_h_set = return_items_with_min_support_p1(item_set,transaction_list,min_support,freq_set)
+    ##DEBUG##
     #print("one_c_set: ",one_c_set)
     #print("two_h_Set: ",two_h_set)
+    ##DEBUG##
     current_l_set = one_c_set
     k = 2
     current_h_set = two_h_set.copy()
     hash_set[k] = two_h_set.copy()
     #while(get_h_min_support_len(hash_set[k],min_support,transaction_list) >= large):
-    #print("current_h_set: ",current_h_set)
+    print("current_h_set: ",current_h_set)
     current_c_set = gen_candidate(current_l_set, current_h_set ,k, min_support,transaction_list) # gen_candidate
-    print("current_c_set: ",current_c_set)
+    print(current_c_set)
     while(current_l_set != set([])):
         large_set[k-1] = current_l_set
         current_l_set = join_set(current_l_set, k) # new candidates
@@ -132,17 +159,23 @@ def run_AprioriDHP(data_iter, min_support, min_confidence,large):
     for key, value in large_set.items():
         to_ret_items.extend([(tuple(item), get_support(item))
                            for item in value])
+    ##DEBUG##
     #print "to_ret_items"
     #print to_ret_items
+    ##DEBUG##
     to_ret_rules = []
+    ##DEBUG##
     #print("large_set.items(): ")
     #print(list(large_set.items())[1:])
+    ##DEBUG##
     #for key, value in large_set.items()[1:]:
     for key, value in list(large_set.items())[1:]:
         for item in value:
             _subsets = map(frozenset, [x for x in subsets(item)])
+            ##DEBUG##
             #print "_subsets:"
             #print _subsets
+            ##DEBUG##
             for element in _subsets:
                 remain = item.difference(element)
                 if len(remain) > 0:
@@ -165,12 +198,16 @@ def print_results(items, rules):
 
 
 def dataFromFile(fname):
-    file_iter = open(fname, 'r',encoding='utf-8-sig')
+    file_iter = open(fname, mode='r')
     for line in file_iter:
+        ##DEBUG##
         #print(line)
+        ##DEBUG##
         line = line.strip().rstrip(',') #.replace('\xef\xbb\xbf','').replace('\ufeff1','')
         record = frozenset(line.split(','))
+        ##DEBUG##
         #print("record: ",record)
+        ##DEBUG##
         yield record
 
 
